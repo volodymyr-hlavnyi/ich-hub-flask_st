a2f7f46cb021d6bde6d59357322dd35b989b9145
volodymyr-hlavnyi <volodymyr.hlavnyi@gmail.com>
2024-08-21 11:42:20 +0200

    🎉

--- hw/hw_01.py
+++ hw/hw_01.py
@@ -1,15 +1,19 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return 'Hello, Flask!'
 
- # /user/<name>, который будет возвращать
+
+# /user/<name>, который будет возвращать
 # приветственное сообщение с переданным именем
 @app.route('/user/<name>')
 def hello_user(name):
     return f'Hello, {name}'
 
+
 if __name__ == '__main__':
-   app.run()
+    app.run()
